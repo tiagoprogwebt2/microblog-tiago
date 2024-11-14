ca9cfddec60f73c2e7d409c6aac46a9947a8c93c
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-11-14 14:53:50 -0300

    Script de conexão ao banco

---
+++ includes/conecta.php
@@ -0,0 +1,19 @@
+<?php
+// Parâmetros de acesso ao servidor
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "microblog_aula";
+
+// Acessar a função de conexão ao servidor de BD
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+// Definindo o charset UTF8 para a conexão
+mysqli_set_charset($conexao, "utf8");
+
+// Verificando a conexão
+if($conexao === false){
+    die("Erro: ".mysqli_connect_error());
+} else {
+    echo "Beleza, segue o jogo...";
+}
