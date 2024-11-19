ab6855956e4c2aefaf4c3dd015eac30d87143c43
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-11-19 15:28:02 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -7,9 +7,8 @@ $id = $_GET['id'];
 
 // Chamamos a função para carregar os dados da pessoa através do id
 $dadosUsuario = listarUmUsuario($conexao, $id);
+//var_dump($dadosUsuario)
 ?>
-<pre><?=var_dump($dadosUsuario)?></pre>
-
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
@@ -22,12 +21,12 @@ $dadosUsuario = listarUmUsuario($conexao, $id);
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$dadosUsuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$dadosUsuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
