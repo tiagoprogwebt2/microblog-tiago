1a25921a3b841760fdee81f901824f34147f03e5
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-11-19 16:18:13 -0300

    Função atualizarUsuario

--- includes/funcoes-usuarios.php
+++ includes/funcoes-usuarios.php
@@ -36,3 +36,17 @@ function listarUmUsuario($conexao, $id){
     return mysqli_fetch_assoc($resultado);
 }
 
+
+
+function atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo){
+    $sql = "UPDATE usuarios SET
+                nome = '$nome',
+                email = '$email',
+                senha = '$senha',
+                tipo = '$tipo'
+            WHERE id = $id"; // NÃO ESQUEÇA NUNCA ESSA BAGAÇA! 
+
+    // COPIE E COLE AQUI O MYSQLI_QUERY DA FUNÇÃO inserirUsuario
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
+
