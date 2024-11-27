7cbdb3c5eceb6a61229c4d621d0aa40c7923863d
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-11-27 14:50:14 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -31,7 +31,11 @@ $listaDeNoticias = lerNoticias($conexao, $idUsuario, $tipoUsuario);
 					<tr>
                         <th>Título</th>
                         <th>Data</th>
+
+						<?php if($tipoUsuario === 'admin'){ ?>
                         <th>Autor</th>
+						<?php } ?>
+
 						<th class="text-center">Operações</th>
 					</tr>
 				</thead>
@@ -41,15 +45,17 @@ $listaDeNoticias = lerNoticias($conexao, $idUsuario, $tipoUsuario);
 					<tr>
                         <td> <?=$noticia['titulo']?> </td>
                         <td> <?=formataData($noticia['data'])?> </td>
+						<?php if($tipoUsuario === 'admin'){ ?>
                         <td> <?=$noticia['nome']?> </td>
+						<?php } ?>
 						<td class="text-center">
 							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
+							href="noticia-atualiza.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-pencil"></i> Atualizar
 							</a>
 						
 							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
+							href="noticia-exclui.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
