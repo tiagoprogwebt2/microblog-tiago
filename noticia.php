ec0a2bbc3826d5b7e7a247a47c9e316a2ba34a67
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-11-28 15:28:53 -0300

    Update noticia.php

--- noticia.php
+++ noticia.php
@@ -7,20 +7,18 @@ $idNoticia = $_GET['id'];
 
 // Carregando os dados da notícia usando o ID
 $dadosDaNoticia = lerNoticiaCompleta($conexao, $idNoticia);
-
-var_dump($dadosDaNoticia);
 ?>
-
 
 <div class="row my-1 mx-md-n1">
 
     <article class="col-12">
-        <h2> Título da notícia... </h2>
+        <h2> <?=$dadosDaNoticia['titulo']?> </h2>
         <p class="font-weight-light">
-            <time>Data da notícia...</time> - <span>Autor da notícia</span>
+            <time><?=formataData($dadosDaNoticia['data'])?></time> 
+            - <span><?=$dadosDaNoticia['nome']?></span>
         </p>
-        <img src="imagens/abstrato.jpg" alt="" class="float-start pe-2 img-fluid">
-        <p>Texto da notícia...</p>
+        <img src="imagens/<?=$dadosDaNoticia['imagem']?>" alt="" class="float-start pe-2 img-fluid">
+        <p class="ajusta-texto"><?=$dadosDaNoticia['texto']?></p>
     </article>
     
 
