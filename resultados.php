5f4fb32824e60a612f5e37c2d676b7761385511e
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-12-03 14:36:24 -0300

    Término da busca

--- resultados.php
+++ resultados.php
@@ -7,32 +7,36 @@ $termoDigitado = $_GET['busca'];
 
 // Carregando os dados da busca no banco de dados pelo termo digitado
 $dadosDaBusca = buscar($conexao, $termoDigitado);
-var_dump($dadosDaBusca);
+//var_dump($dadosDaBusca);
 ?>
 
 <div class="row bg-white rounded shadow my-1 py-4">
     <h2 class="col-12 fw-light">
         Você procurou por 
         <span class="badge bg-dark"> <?=$termoDigitado?> </span> e
-        obteve <span class="badge bg-info">X</span> resultados
+        obteve <span class="badge bg-info"> 
+            <?=count($dadosDaBusca)?>
+        </span> resultados
     </h2>
     
+<?php foreach($dadosDaBusca as $noticia){ ?>
     <div class="col-12 my-1">
-
         <article class="card">
             <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
+                <h3 class="fs-4 card-title fw-light">
+                    <?=$noticia['titulo']?>
+                </h3>
                 <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
+                    <time><?=formataData($noticia['data'])?></time> - 
+                    <?=$noticia['resumo']?>
                 </p>
                 
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
+                <a href="noticia.php?id=<?=$noticia['id']?>" 
+                class="btn btn-primary btn-sm">Continuar lendo</a>
             </div>
         </article>
-
     </div>
-
+<?php } ?>
 </div>     
 
 <?php 
